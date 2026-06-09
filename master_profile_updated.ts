0ccd1c8a7695f0a26e3eb81e899b83217d94ada7
voytikyevhen-ip43-bit <voytik.yevhen-ip43@edu.kpi.ua>
2026-06-09 12:29:14 +0300

    Delete master_profile_updated.ts

--- master_profile_updated.ts
+++
@@ -1,9 +0,0 @@
-// contract/events/master-profile-updated.event.ts
-export class MasterProfileUpdatedEvent {
-  constructor(
-    public readonly masterId: string,
-    public readonly displayName: string,
-    public readonly avatarUrl: string,
-    public readonly updatedAt: Date,
-  ) {}
-}
